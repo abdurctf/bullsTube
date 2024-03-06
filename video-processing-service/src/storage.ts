9d2c8942df0845b534b93969380ef66829578c33
abdurctf <asharif1@usf.edu>
2024-03-06 03:50:54 -0500

    processed video is updated in the firestore too

--- video-processing-service/src/storage.ts
+++ video-processing-service/src/storage.ts
@@ -1,4 +1,4 @@
-import { Storage } from "@google-cloud/storage";
+import { Storage } from '@google-cloud/storage';
 import fs from 'fs';
 import ffmpeg from 'fluent-ffmpeg';
 
@@ -28,16 +28,16 @@ export function setupDirectories() {
 export function convertVideo(rawVideoName: string, processedVideoName: string) {
   return new Promise<void>((resolve, reject) => {
     ffmpeg(`${localRawVideoPath}/${rawVideoName}`)
-      .outputOptions("-vf", "scale=-1:360") // 360p
-      .on("end", function () {
-        console.log("Processing finished successfully");
-        resolve();
-      })
-      .on("error", function (err: any) {
-        console.log("An error occurred: " + err.message);
-        reject(err);
-      })
-      .save(`${localProcessedVideoPath}/${processedVideoName}`);
+    .outputOptions("-vf", "scale=-1:360") // 360p
+    .on("end", () => {
+      console.log("Processing finished successfully.");
+      resolve();
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      reject(err);
+    })
+    .save(`${localProcessedVideoPath}/${processedVideoName}`);
   });
 }
 
@@ -50,13 +50,11 @@ export function convertVideo(rawVideoName: string, processedVideoName: string) {
 export async function downloadRawVideo(fileName: string) {
   await storage.bucket(rawVideoBucketName)
     .file(fileName)
-    .download({
-      destination: `${localRawVideoPath}/${fileName}`,
-    });
+    .download({ destination: `${localRawVideoPath}/${fileName}` });
 
   console.log(
     `gs://${rawVideoBucketName}/${fileName} downloaded to ${localRawVideoPath}/${fileName}.`
-  );
+  )
 }
 
 
@@ -68,16 +66,13 @@ export async function downloadRawVideo(fileName: string) {
 export async function uploadProcessedVideo(fileName: string) {
   const bucket = storage.bucket(processedVideoBucketName);
 
-  // Upload video to the bucket
-  await storage.bucket(processedVideoBucketName)
-    .upload(`${localProcessedVideoPath}/${fileName}`, {
-      destination: fileName,
-    });
+  await bucket.upload(`${localProcessedVideoPath}/${fileName}`, {
+    destination: fileName
+  });
   console.log(
     `${localProcessedVideoPath}/${fileName} uploaded to gs://${processedVideoBucketName}/${fileName}.`
   );
 
-  // Set the video to be publicly readable
   await bucket.file(fileName).makePublic();
 }
 
@@ -113,20 +108,19 @@ function deleteFile(filePath: string): Promise<void> {
     if (fs.existsSync(filePath)) {
       fs.unlink(filePath, (err) => {
         if (err) {
-          console.error(`Failed to delete file at ${filePath}`, err);
+          console.log(`Failed to delete file at ${filePath}`, err);
           reject(err);
         } else {
           console.log(`File deleted at ${filePath}`);
           resolve();
         }
-      });
+      })
     } else {
-      console.log(`File not found at ${filePath}, skipping delete.`);
+      console.log(`File not found at ${filePath}, skipping the delete.`);
       resolve();
     }
   });
 }
-
 
 /**
  * Ensures a directory exists, creating it if necessary.
