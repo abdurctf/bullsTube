6ee0cc7f4ad2a59cfbd4ed585b7b5c304996ba74
abdurctf <asharif1@usf.edu>
2024-03-15 03:04:25 -0400

    client side deployed on cloud

--- web-client/app/watch/page.tsx
+++ web-client/app/watch/page.tsx
@@ -1,15 +1,24 @@
 'use client';
 
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
-export default function Watch() {
+function WatchComponent() {
   const videoPrefix = 'https://storage.googleapis.com/bullstube-processed-videos/';
-  const videoSrc = useSearchParams().get('v');
+  const videoSrc = useSearchParams().get('v') ?? '';
 
   return (
     <div>
       <h1>Watch Page</h1>
       <video controls src={videoPrefix + videoSrc}/>
     </div>
+  );
+}
+
+export default function Watch() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <WatchComponent />
+    </Suspense>
   );
 }
